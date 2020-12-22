6f907f56bea53a237f2262b4b08cfc3567fd9216
Mark Lorenz <dicotomygames@gmail.com>
2020-12-21 21:23:46 -0500

    GPIO TESTING

--- octoprint_OctoBuddy/__init__.py
+++ octoprint_OctoBuddy/__init__.py
@@ -5,9 +5,18 @@ from __future__ import absolute_import, unicode_literals
 import octoprint.plugin
 import RPi.GPIO as GPIO
 
+def button_callback(channel):
+    print("Button was pushed!")
+
 class OctoBuddyPlugin(octoprint.plugin.StartupPlugin):
         def on_after_startup(self):
                 self._logger.info("OctoBuddy Alive Now!")
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(8, GPIO.RISING, callback=button_callback)
 
 __plugin_pythoncompat__ = ">=2.7,<4"
 __plugin_implementation__ = OctoBuddyPlugin()
