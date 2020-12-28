2a8f5b5d3f2521096b8b9e73281823db44bf7544
Mark Lorenz <dicotomygames@gmail.com>
2020-12-27 23:26:25 -0500

    asv

--- octoprint_OctoBuddy/__init__.py
+++ octoprint_OctoBuddy/__init__.py
@@ -18,8 +18,7 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(11, GPIO.RISING, callback=button_callback, bouncetime = 200)
-
-
+self._logger.info("Does This Work?")
 
 __plugin_pythoncompat__ = ">=2.7,<4"
 __plugin_implementation__ = OctoBuddyPlugin()
