7a64085b005530957ebc1f883e2281e000247a49
Mark Lorenz <dicotomygames@gmail.com>
2020-12-27 22:49:01 -0500

    vsdvasd

--- octoprint_OctoBuddy/__init__.py
+++ octoprint_OctoBuddy/__init__.py
@@ -7,6 +7,7 @@ import RPi.GPIO as GPIO
 
 def button_callback(channel):
     print("Button was pushed!")
+	self._logger.info("Button Was Pushed!!!!")
 
 class OctoBuddyPlugin(octoprint.plugin.StartupPlugin):
         def on_after_startup(self):
@@ -17,6 +18,7 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(8, GPIO.RISING, callback=button_callback)
+
 
 __plugin_pythoncompat__ = ">=2.7,<4"
 __plugin_implementation__ = OctoBuddyPlugin()
