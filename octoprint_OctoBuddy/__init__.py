f241cfa0cc684574d25c4f3824e7f48c55c753a8
Mark Lorenz <dicotomygames@gmail.com>
2020-12-27 23:49:19 -0500

    vsdvds

--- octoprint_OctoBuddy/__init__.py
+++ octoprint_OctoBuddy/__init__.py
@@ -18,8 +18,8 @@ class OctoBuddyPlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.ShutdownP
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-#GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-#GPIO.add_event_detect(11, GPIO.RISING, callback=button_callback, bouncetime = 200)
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(11, GPIO.RISING, callback=button_callback, bouncetime = 200)
 
 __plugin_pythoncompat__ = ">=2.7,<4"
 __plugin_implementation__ = OctoBuddyPlugin()
