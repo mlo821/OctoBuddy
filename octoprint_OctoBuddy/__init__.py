286827acd00529e6b29678eb2a10cf8db8c7d90a
Mark Lorenz <dicotomygames@gmail.com>
2020-12-27 22:20:20 -0500

    Remote GPIO

--- octoprint_OctoBuddy/__init__.py
+++ octoprint_OctoBuddy/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import absolute_import, unicode_literals
 
 import octoprint.plugin
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
 def button_callback(channel):
     print("Button was pushed!")
@@ -13,10 +13,10 @@ class OctoBuddyPlugin(octoprint.plugin.StartupPlugin):
                 self._logger.info("OctoBuddy Alive Now!")
 
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(8, GPIO.RISING, callback=button_callback)
+#GPIO.setwarnings(False)
+#GPIO.setmode(GPIO.BOARD)
+#GPIO setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#GPIO.add_event_detect(8, GPIO.RISING, callback=button_callback)
 
 __plugin_pythoncompat__ = ">=2.7,<4"
 __plugin_implementation__ = OctoBuddyPlugin()
