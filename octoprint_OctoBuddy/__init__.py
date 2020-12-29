10458b85c08ec38ec4627c3e093d3070986271d4
Mark Lorenz <dicotomygames@gmail.com>
2020-12-29 11:57:31 -0500

    sdv

--- octoprint_OctoBuddy/__init__.py
+++ octoprint_OctoBuddy/__init__.py
@@ -40,7 +40,7 @@ class OctoBuddyPlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.ShutdownP
         self._logger.info("OctoBuddy Going to Bed Now!")
         self._logger.info(buttonpressed)
 
-    def button_callback(channel):
+    def button_callback(self, channel):
         self._logger.info("Button Pressed")
 
 
