71cbfacd302fc5e8597d630007be3470a19e8fdd
Mark Lorenz <dicotomygames@gmail.com>
2020-12-29 17:14:37 -0500

    vsdv

--- octoprint_OctoBuddy/__init__.py
+++ octoprint_OctoBuddy/__init__.py
@@ -19,7 +19,7 @@ class OctoBuddyPlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.ShutdownP
         GPIO.add_event_detect(11, GPIO.BOTH, callback=self.button_callback, bouncetime = 100)
         #GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         self._logger.info(GPIO.RPI_INFO)
-		self.testLogging("This is a message")
+        self.testLogging("This is a message")
         #GPIO.add_event_detect(12, GPIO.BOTH, callback=button_callback, bouncetime = 100)
         #GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         #GPIO.add_event_detect(13, GPIO.BOTH, callback=button_callback, bouncetime = 100)
