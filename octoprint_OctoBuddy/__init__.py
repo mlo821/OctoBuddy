040a9efa08ca2a1ab8e0fdf515708416d974b045
Mark Lorenz <dicotomygames@gmail.com>
2020-12-29 11:42:20 -0500

    sadf

--- octoprint_OctoBuddy/__init__.py
+++ octoprint_OctoBuddy/__init__.py
@@ -13,16 +13,6 @@ class OctoBuddyPlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.ShutdownP
     def on_after_startup(self):
         self._logger.info("OctoBuddy Alive Now!")
         self._logger.info(buttonpressed)
-
-    def on_shutdown(self):
-        GPIO.cleanup();
-        self._logger.info("OctoBuddy Going to Bed Now!")
-        self._logger.info(buttonpressed)
-
-    def button_callback(channel):
-        self._logger.info("Button Pressed")
-
-    def setup_GPIO():
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -44,6 +34,15 @@ class OctoBuddyPlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.ShutdownP
         GPIO.add_event_detect(23, GPIO.BOTH, callback=button_callback, bouncetime = 100)
         GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.add_event_detect(24, GPIO.BOTH, callback=button_callback, bouncetime = 100)
+
+    def on_shutdown(self):
+        GPIO.cleanup();
+        self._logger.info("OctoBuddy Going to Bed Now!")
+        self._logger.info(buttonpressed)
+
+    def button_callback(channel):
+        self._logger.info("Button Pressed")
+
 
 __plugin_pythoncompat__ = ">=2.7,<4"
 __plugin_implementation__ = OctoBuddyPlugin()
