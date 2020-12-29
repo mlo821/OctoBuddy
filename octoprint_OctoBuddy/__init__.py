1d966f8d34ee93a25114b158583ae2c8df6bfc76
Mark Lorenz <dicotomygames@gmail.com>
2020-12-29 17:47:04 -0500

    lkjl

--- octoprint_OctoBuddy/__init__.py
+++ octoprint_OctoBuddy/__init__.py
@@ -19,7 +19,6 @@ class OctoBuddyPlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.ShutdownP
         GPIO.add_event_detect(11, GPIO.BOTH, callback=self.testLogging, bouncetime = 100)
         #GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         self._logger.info(GPIO.RPI_INFO)
-        self.testLogging("Sending a message from startup")
         #GPIO.add_event_detect(12, GPIO.BOTH, callback=button_callback, bouncetime = 100)
         #GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         #GPIO.add_event_detect(13, GPIO.BOTH, callback=button_callback, bouncetime = 100)
