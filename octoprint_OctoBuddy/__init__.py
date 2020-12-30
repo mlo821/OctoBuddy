bc5e7a2fd3852afcc13e68e9b9b6a66593b20251
Mark Lorenz <dicotomygames@gmail.com>
2020-12-30 16:37:08 -0500

    sav

--- octoprint_OctoBuddy/__init__.py
+++ octoprint_OctoBuddy/__init__.py
@@ -36,14 +36,17 @@ class OctoBuddyPlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.ShutdownP
         self._logger.info("OctoBuddy Going to Bed Now!")
         self._logger.info(buttonpressed)
 
-    def button_callback(channel):
+    def button_callback(self, channel):
         newself = self
         newself._logger.debug("asshole work")
 
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(22, GPIO.RISING, callback=button_callback, bouncetime = 400)
+    def setup_GPIO(self)
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(22, GPIO.RISING, callback=self.button_callback, bouncetime = 400)
+
+     setup_GPIO()
 
 __plugin_pythoncompat__ = ">=2.7,<4"
 __plugin_implementation__ = OctoBuddyPlugin()
