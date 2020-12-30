7520528da8e86508c554222132ec889d0cdb1def
Mark Lorenz <dicotomygames@gmail.com>
2020-12-30 17:26:01 -0500

    kjhgk

--- octoprint_OctoBuddy/__init__.py
+++ octoprint_OctoBuddy/__init__.py
@@ -44,7 +44,7 @@ class OctoBuddyPlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.ShutdownP
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.add_event_detect(22, GPIO.RISING, callback=self.button_callback, bouncetime = 400)
+        GPIO.add_event_detect(22, GPIO.RISING, callback=self.button_callback(self), bouncetime = 400)
 
 __plugin_pythoncompat__ = ">=2.7,<4"
 __plugin_implementation__ = OctoBuddyPlugin()
