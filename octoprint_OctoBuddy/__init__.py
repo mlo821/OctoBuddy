a80bcceb56565daf351ce0acfe22ad482abe9c07
Mark Lorenz <dicotomygames@gmail.com>
2020-12-30 18:18:48 -0500

    tewt

--- octoprint_OctoBuddy/__init__.py
+++ octoprint_OctoBuddy/__init__.py
@@ -4,11 +4,12 @@ from __future__ import absolute_import, unicode_literals
 import octoprint.plugin
 import RPi.GPIO as GPIO
 
-buttonpressed = False
+
 import os
 
 
 class OctoBuddyPlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.ShutdownPlugin):
+
     def on_after_startup(self):
         self._logger.info("OctoBuddy Alive Now!")
         self._logger.info(buttonpressed)
@@ -16,35 +17,24 @@ class OctoBuddyPlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.ShutdownP
         self._logger.info(GPIO.RPI_INFO)
         self._setup_sensor()
         self.setup_GPIO();
-        #GPIO.add_event_detect(12, GPIO.BOTH, callback=button_callback, bouncetime = 100)
-        #GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        #GPIO.add_event_detect(13, GPIO.BOTH, callback=button_callback, bouncetime = 100)
-        #GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        #GPIO.add_event_detect(15, GPIO.BOTH, callback=button_callback, bouncetime = 100)
-        #GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        #GPIO.add_event_detect(16, GPIO.BOTH, callback=button_callback, bouncetime = 100)
-        #GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        #GPIO.add_event_detect(21, GPIO.BOTH, callback=button_callback, bouncetime = 100)
-        #GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        #GPIO.add_event_detect(22, GPIO.BOTH, callback=button_callback, bouncetime = 100)
-        #GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        #GPIO.add_event_detect(23, GPIO.BOTH, callback=button_callback, bouncetime = 100)
-        #GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        #GPIO.add_event_detect(24, GPIO.BOTH, callback=button_callback, bouncetime = 100)
 
     def on_shutdown(self):
         GPIO.cleanup();
         self._logger.info("OctoBuddy Going to Bed Now!")
-        self._logger.info(buttonpressed)
+        self._logger.info("Test")
 
-    def button_callback(self, channel):
-        self._logger.debug("asshole work")
+    def button_callback(self):
+        self._logger.info("test")
+        self._logger.info("and I have to type this again")
 
     def setup_GPIO(self):
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.add_event_detect(22, GPIO.RISING, callback=self.button_callback(self), bouncetime = 400)
+
+
+
 
 __plugin_pythoncompat__ = ">=2.7,<4"
 __plugin_implementation__ = OctoBuddyPlugin()
