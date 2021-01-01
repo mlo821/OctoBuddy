fe36a65676e68f94e965a8e76f182596984b647d
Mark Lorenz <dicotomygames@gmail.com>
2021-01-01 12:31:31 -0500

    efef

--- octoprint_OctoBuddy/__init__.py
+++ octoprint_OctoBuddy/__init__.py
@@ -16,29 +16,27 @@ class OctoBuddyPlugin(octoprint.plugin.StartupPlugin,
 
     def on_after_startup(self):
         self._logger.info("OctoBuddy Alive Now!")
-        self._logger.info(self._printer.get_state_id())
-        self._logger.info(GPIO.RPI_INFO)
-        self.setup_GPIO()
+    #    self._logger.info(self._printer.get_state_id())
+    #    self._logger.info(GPIO.RPI_INFO)
+    #    self.setup_GPIO()
 
     def on_shutdown(self):
-        GPIO.cleanup()
-        self._logger.info("OctoBuddy Going to Bed Now!")
+    #    GPIO.cleanup()
+    #    self._logger.info("OctoBuddy Going to Bed Now!")
 
-    def button_callback(self, channel):
-        self._logger.info(channel + "Pressed")
+    #def button_callback(self, channel):
+    #    self._logger.info(channel + "Pressed")
 
-        if channel == home_pin:
-            self._printer.home("x")
-            self._printer.home("y")
-            self._printer.home("z")
+    #    if channel == home_pin:
+    #        self._printer.home("x")
+    #        self._printer.home("y")
+    #        self._printer.home("z")
 
-    def setup_GPIO(self):
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.add_event_detect(22, GPIO.RISING, callback=self.button_callback, bouncetime = self.debounce)
-        GPIO.setup(self.home_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.add_event_detect(self.home_pin, GPIO.RISING, callback=self.button_callback, bouncetime = self.debounce)
+    #def setup_GPIO(self):
+    #    GPIO.setwarnings(False)
+    #    GPIO.setmode(GPIO.BOARD)
+    #    GPIO.setup(self.home_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    #    GPIO.add_event_detect(self.home_pin, GPIO.RISING, callback=self.button_callback, bouncetime = self.debounce)
 
     def get_settings_defaults(self):
         return dict(
@@ -49,13 +47,13 @@ class OctoBuddyPlugin(octoprint.plugin.StartupPlugin,
     def get_template_configs(self):
         return [dict(type = "settings", custom_bindings=False)]
 
-    @property
-    def debounce(self):
-        return int(self._settings.get(["debounce"]))
+    #@property
+    #def debounce(self):
+    #    return int(self._settings.get(["debounce"]))
 
-    @property
-    def home_pin(self):
-        return int(self._settings.get(["home_pin"]))
+    #@property
+    #def home_pin(self):
+     #   return int(self._settings.get(["home_pin"]))
 
     def on_settings_save(self):
         octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
